d8917385916af6df4dd37850a552ba7bfb2c7e9b
lerwine <lenny@erwinefamily.net>
2018-06-23 22:25:50 -0400

    Applied bootstrap theme. Added lib for bootstrap and angular.

---
+++ script/exampleController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('exampleApp')
+        .controller('exampleController', controller1);
+
+    controller1.$inject = ['$scope']; 
+
+    function controller1($scope) {
+        $scope.title = 'exampleController';
+        
+        $scope.firstName = 'Leonard';
+        $scope.lastName = 'Erwine';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ script/exampleController2.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('exampleApp')
+        .controller('exampleController2', controller);
+
+    controller.$inject = ['$location']; 
+
+    function controller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'controller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
