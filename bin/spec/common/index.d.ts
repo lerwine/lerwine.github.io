140d8039c5010e10f57f843ee1ea3308d02586ff
lerwine <lenny@erwinefamily.net>
2018-07-29 21:56:45 -0400

    Moved source and build scripts back to MyGitHubPages repository

--- bin/spec/common/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface ITestData<TTarget, TExpected> {
-    target: TTarget;
-    expected: TExpected;
-    argsDiplayText?: string | string[];
-}
-//# sourceMappingURL=index.d.ts.map
--- test.js
+++
@@ -1,9 +0,0 @@
-var FS = require("fs");
-var Path = require("path");
-var util = require('util');
-var os = require('os');
-
-var re2 = /[\t\x20-\xfe]*((?:(?:\r\n)+[\t\x20-\xfe]+)+(?:\r\n)*)?/;
-var re = /[\t\x20-\xfe]*((?:\r\n)+(?:[\t\x20-\xfe]+(?:(?:\r\n)+[\t\x20-\xfe]+)*(?:\r\n)*)?)?/;
-
-console.log(re.source);
